29be6a7d3665631687e4f323c089da323f8fc353
Sanyam-02 <sanyamagrawal7431@gmail.com>
2021-10-30 22:56:49 +0530

    Final Commit

---
+++ Sanyam/include/2darray.h
@@ -0,0 +1,6 @@
+#ifndef _HELLO_H
+#define _HELLO_H
+
+int** sgs_clib_2d_int_array(int size_of_array);
+
+#endif
---
+++ Sanyam/src/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"../include/2darray.h"
+
+int main(){
+    printf("Enr. No.-0801CS201082 \n");
+    sgs_clib_2d_int_array(5);
+    return 0;
+}
