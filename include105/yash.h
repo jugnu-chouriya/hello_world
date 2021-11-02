10b41f1880cb79334481aeae6ad5a34e4e66018a
Yashtitoriya <ytitoriya12345@gmail.com>
2021-11-02 18:44:47 +0530

    Adding a src105 and include105  files to new_branch
    Signed-off-by: Yashtitoriya <ytitoriya12345@gmail.com>

---
+++ include105/yash.h
@@ -0,0 +1,6 @@
+#ifndef _YASH_H
+#define _YASH_H
+
+void sgs_clib_convert_to_lower(char *message);
+
+#endif
---
+++ src105/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "..\include105\yash.h"
+
+void main(){
+
+    char c[]="YashTitoriya";
+    sgs_clib_convert_to_lower(c);  
+
+    printf(" Converting : %s",c);
+
+}
