b343d3c4b4ab5755088863421b6e0618d2d8c629
Samiksha-Ra <samiksharathore1307@gmail.com>
2021-11-03 00:42:46 +0530

    0801CS201078

---
+++ samiksha/include/header.h
@@ -0,0 +1,6 @@
+#ifndef _HEADER_H
+#define _HEADER_H
+
+double * sgs_clib_1d_double_array(size_t size_of_array);
+
+#endif 
---
+++ samiksha/src/main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include"../include/header.h"
+void main(){
+    printf("En. No.: 0801CS201078 \n");
+    size_t size=3;
+    sgs_clib_1d_double_array(size);
+}
