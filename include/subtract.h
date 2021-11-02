36081996a794f469f8d51207aa4e275612067eb1
Aniket979Kanodia <kanodiaaniket979@gmail.com>
2021-11-02 18:20:48 +0530

    Completes Assignment by Aniket Kanodia 0801CS201013

---
+++ include/subtract.h
@@ -0,0 +1,7 @@
+#ifndef _HELLO_H
+#define _HELLO_H
+
+
+void sgs_clib_subtract(int num1,int num2);
+
+#endif
---
+++ src/subtract.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "../include/subtract.h"
+
+
+void sgs_clib_subtract(int num1,int num2){
+int num = num2 - num1;
+printf("%d",num);
+}
+
