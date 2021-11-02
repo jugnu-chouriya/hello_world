72879cdfd06d8367d0bdce9938eab498adf06fd9
Gaurav Bairagi <gauravbairagi109@gmail.com>
2021-11-02 13:51:17 +0530

    committed by gaurav bairagi

---
+++ Gauravbairagi_0801cs10/src/hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include "C:\Users\gaura\Desktop\gauravbairagi_0801CS201032\INCLUDE\hello.h"
+
+int sgs_clib_remainder(int number_1, int number_2){  
+  int r;
+  r=number_1%number_2;
+  printf("%d",r);
+  return 0;
+   
+
+}
