625a1f74bcf1bd40fbfa1e9525f684e57414538b
Manal0 <manalchoudhary8@gmail.com>
2021-11-02 17:52:06 +0530

    0801CS201053

--- ManalChoudhary/include/strlen.h
+++ ManalChoudhary/include/strlen.h
@@ -1,5 +1,5 @@
 #ifndef _strlen_H
 #define _strlen_H
 
-size_t sgs_clib_strlen(const char *s);
+size_t sgs_clib_strlen(const char *str);
 #endif
--- ManalChoudhary/src/strlen.c
+++ ManalChoudhary/src/strlen.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include "../include/strlen.h"
-size_t sgs_clib_strlen(const char *s)
+size_t sgs_clib_strlen(const char *str)
 {
     size_t count = 0;
-    while(*s!='\0')
+    while(*str!='\0')
     {
         count++;
-        s++;
+        str++;
     }
     return count;
 }
