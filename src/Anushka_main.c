83a661b74b6d5e231187d11dab27d4b5d25cc72c
Anushka-Gamad <86728397+Anushka-Gamad@users.noreply.github.com>
2021-11-02 22:15:09 -0700

    Fix: Added multiply feature

---
+++ src/Anushka_main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include "..\include\Anushka_multiply.h"
+
+int main(){
+    
+    printf("Multiplication:%d",sgs_clib_multiply(3,4));
+    return 0;
+}
---
+++ src/Anushka_multiply.c
@@ -0,0 +1,6 @@
+#include "..\include\Anushka_multiply.h"
+#include <stdio.h>
+
+int sgs_clib_multiply(int number_1, int number_2){
+    return number_1*number_2;
+}
